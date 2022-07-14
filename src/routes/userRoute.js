97d4e7e20b586f7266f1311233ed0b1a50f0f15f
André Melo <adr.smelo@gmail.com>
2022-07-14 15:50:28 -0300

    feat: (userRoute) implementa rota para o endpoint DELETE /user/me
    junto a rota desse endpoint foi implementado o middleware de validação do token (validateJWT)

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -7,6 +7,7 @@ const route = express.Router();
 
 route.get('/', validateJWT, user.getAll);
 route.post('/', validUser, user.create);
+route.delete('/me', validateJWT, user.remove);
 route.get('/:id', validateJWT, user.getById);
 
 module.exports = route;
