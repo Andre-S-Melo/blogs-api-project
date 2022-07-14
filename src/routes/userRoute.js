df2a24d7a8528d58bfdda5539087cdd1a5e3650f
André Melo <adr.smelo@gmail.com>
2022-07-14 14:40:22 -0300

    feat: (validateJWT) implementa middleware de validação do token junto a rota do endpoint GET /user

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const user = require('../controllers/userController');
+const validateJWT = require('../middlewares/validateJWT');
 const { validUser } = require('../middlewares/userValidation');
 
 const route = express.Router();
 
-route.get('/', user.getAll);
+route.get('/', validateJWT, user.getAll);
 route.post('/', validUser, user.create);
 
 module.exports = route;
