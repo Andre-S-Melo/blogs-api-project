dd7f63cbda35dc2d26080aac063287721b8b3824
André Melo <adr.smelo@gmail.com>
2022-07-14 17:37:22 -0300

    feat: (POST /categories) implementa endpoint POST /categories

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,17 @@
+const categoryService = require('../services/categoryService');
+
+const create = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+
+    const newCategory = await categoryService.create({ name });
+
+    return res.status(201).json(newCategory);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  create,
+};
