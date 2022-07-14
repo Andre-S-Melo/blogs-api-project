f79270e82e6da45eec3d2ad03a88120c4a997c2b
André Melo <adr.smelo@gmail.com>
2022-07-13 23:07:50 -0300

    feat: (userRoute) implementa rota para o endpoint POST /user

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const user = require('./routes/userRoute');
 const login = require('./routes/loginRoute');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/user', user);
 app.use('/login', login);
 
 app.use(errorHandler);
