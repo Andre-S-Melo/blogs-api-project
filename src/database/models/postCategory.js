93cc26ca2db9ad892d2c684db5ed22981b03c2b4
André Melo <adr.smelo@gmail.com>
2022-07-14 19:56:05 -0300

    feat: (postCategoryModel) implementa model postCategory junto a camada Models

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,27 @@
+const createPostCategory = (sequelize, _DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory',
+    {}, {
+    timestamps: false,
+    tableName: 'PostCategories',
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogposts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = createPostCategory;
