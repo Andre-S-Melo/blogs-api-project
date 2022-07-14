61fda8b1c77b268578aa0bef130bd7fd19d95b91
André Melo <adr.smelo@gmail.com>
2022-07-13 21:49:19 -0300

    feat: (loginRoute) implementa rota para o endpoint POST /login

--- src/api.js
+++ src/api.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const login = require('./routes/loginRoute');
 const errorHandler = require('./middlewares/errorHandler');
-
-// ...
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use('/login', login);
 
 app.use(errorHandler);
 
