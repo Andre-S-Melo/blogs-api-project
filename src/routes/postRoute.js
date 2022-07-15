0229b8e3aec10585390c344046aa2554371a2360
André Melo <adr.smelo@gmail.com>
2022-07-15 19:43:18 -0300

    feat: (postRoute) implementa rota para o endpoint DELETE /post/:id
    junto a rota desse endpoint foi implementado o middleware de validação do token (validateJWT)

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -8,5 +8,6 @@ const route = express.Router();
 route.get('/', validateJWT, post.getAll);
 route.get('/:id', validateJWT, post.getById);
 route.put('/:id', validateJWT, validUpdate, post.update);
+route.delete('/:id', validateJWT, post.remove);
 
 module.exports = route;
