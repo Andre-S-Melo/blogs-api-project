a4f583345e333b1ee059778c544c637a3602a35e
André Melo <adr.smelo@gmail.com>
2022-07-15 17:28:51 -0300

    feat: (GET /post/:id) implementa endpoint GET /post/:id

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -10,6 +10,19 @@ const getAll = async (_req, res, next) => {
   }
 };
 
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const post = await postService.getById(+id);
+
+    return res.status(200).json(post);
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   getAll,
+  getById,
 };
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,3 +1,4 @@
+const err = require('../utils/error');
 const { User, BlogPost, Category } = require('../database/models');
 
 const getAll = async () => {
@@ -9,6 +10,19 @@ const getAll = async () => {
   return posts;
 };
 
+const getById = async (id) => {
+  const post = await BlogPost.findOne({
+    where: { id },
+    include: [{ model: User, as: 'user', attributes: { exclude: ['password'] } },
+    { model: Category, as: 'categories', through: { attributes: [] } }],
+  });
+
+  if (!post) throw err(404, 'Post does not exist');
+
+  return post;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
