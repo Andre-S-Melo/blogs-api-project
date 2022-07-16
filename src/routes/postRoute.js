e95bb3b4b25d220d8bc72b0c3326a5e4cebb6ff2
André Melo <adr.smelo@gmail.com>
2022-07-15 21:35:13 -0300

    feat: (postRoute) implementa rota para o endpoint POST /post
    junto a rota desse endpoint foi implementado o middleware de validação do token (validateJWT)

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const post = require('../controllers/postController');
 const validateJWT = require('../middlewares/validateJWT');
-const validUpdate = require('../middlewares/postValidation');
+const { validPost, validUpdate } = require('../middlewares/postValidation');
 
 const route = express.Router();
 
 route.get('/', validateJWT, post.getAll);
+route.post('/', validateJWT, validPost, post.create);
 route.get('/search', validateJWT, post.getBySearch);
 route.get('/:id', validateJWT, post.getById);
 route.put('/:id', validateJWT, validUpdate, post.update);
